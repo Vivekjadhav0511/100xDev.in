f488def1441a99641484a1703c10f1ca87719ef9
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-09-03 16:05:35 +0530

    Week01

---
+++ 02.Week Asynchronous Javascript, Callbacks/Function&Callback/01.AysncJavaScript.js
@@ -0,0 +1,98 @@
+// Normal functions in JS
+
+function sum(a, b) {
+	return a + b;
+}
+
+let ans = sum(2, 3)
+console.log(sum);
+
+
+// Find sum from 1 to a number
+
+function sumFromOneToNumber(n) {
+	let ans = 0;
+	for (let i = 1; i <= n; i++) {
+		ans = ans + i
+	}
+	return ans;
+}
+
+const ans2 = sumFromOneToNumber(100);
+console.log(ans2);
+
+
+// Synchronous code
+
+function sum(n) {
+	let ans = 0;
+	for (let i = 1; i <= n; i++) {
+		ans = ans + i
+	}
+	return ans;
+}
+
+const ans1 = sum(100);
+console.log(ans1);
+const ans3 = sum(10000);
+console.log(ans3);
+
+
+// I/O heavy operations ( Synchronously (One by one))
+
+const fs = require("fs");
+
+const contents = fs.readFileSync("a.txt", "utf-8");
+console.log(contents);
+
+const contents2 = fs.readFileSync("b.txt", "utf-8");
+console.log(contents2);
+
+const contents3 = fs.readFileSync("b.txt", "utf-8");
+console.log(contents3);
+
+
+// Functional arguments
+
+function sum(a, b) {
+    return a + b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function doOperation(a, b, op) {
+    return op(a, b)
+  }
+  
+  console.log(doOperation(1, 2, sum))
+
+
+//   Asynchronous code, callbacks
+
+function run() {
+	console.log("I will run after 1s");
+}
+
+setTimeout(run, 1000);
+console.log("I will run immedietely");
+
+// JS Architecture for async code
+
+function first() {
+    console.log("First");
+  }
+  function second() {
+    first();
+    console.log("Second");
+  }
+  second();
---
+++ 02.Week Asynchronous Javascript, Callbacks/LeactureSlides.md
@@ -0,0 +1,8 @@
+
+## Normal Function,CallBack,Functional arguments & Asynchronous code
+
+[https://projects.100xdevs.com/tracks/async-js-1/Asynchronous-Javascript--Callbacks-and-more-1](Normal Function & CallBack)
+
+How JS Executes Asynchronous Code -`http://latentflip.com/loupe/`
+
+Good Talk - Concurrency is Not Parallelism  - `https://www.youtube.com/watch?v=oV9rvDllKEg`
