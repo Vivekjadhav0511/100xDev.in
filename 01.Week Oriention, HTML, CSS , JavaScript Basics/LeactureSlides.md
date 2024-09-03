b5af9003cc1feef94b9c525aa7949c5af14607d8
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-09-03 12:29:56 +0530

    Orientation Basics Of HTML CSS & JS

---
+++ 01.Week Oriention, HTML, CSS , JavaScript Basics/LeactureSlides.md
@@ -0,0 +1,5 @@
+
+## Oriention Leacture Slides 
+
+[https://projects.100xdevs.com/tracks/async-js-1/Asynchronous-Javascript--Callbacks-and-more-1](Orientaion Basics Of HTML CSS & JavaScript)
+
--- 02.WEEK/01.AysncJavaScript.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const contents = fs.readFile("a.txt", "utf-8"); // Asynchronusly
-console.log(contents);
-
-
-const contentsOfb = fs.readFileSync("b.txt","utf-8")  // Synchronusly
-console.log(contentsOfb);
-
