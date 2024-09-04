9c4477f51e6853b6f9613447f9b1c19873670b97
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-09-04 13:46:08 +0530

    Normal Function,CallBack,Functional arguments & Asynchronous code

--- 02.Week Asynchronous Javascript, Callbacks/01.Function&Callback/AysncJavaScript.js
+++ 02.Week Asynchronous Javascript, Callbacks/01.Function&Callback/AysncJavaScript.js
@@ -45,10 +45,10 @@ const fs = require("fs");
 const contents = fs.readFileSync("a.txt", "utf-8");
 console.log(contents);
 
-const contents2 = fs.readFileSync("b.txt", "utf-8");
+const contents2 = fs.readFileSync("aa.txt", "utf-8");
 console.log(contents2);
 
-const contents3 = fs.readFileSync("b.txt", "utf-8");
+const contents3 = fs.readFileSync("aa.txt", "utf-8");
 console.log(contents3);
 
 
