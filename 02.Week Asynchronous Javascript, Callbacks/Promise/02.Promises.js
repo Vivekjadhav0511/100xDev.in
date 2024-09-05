c8e30364c48037a30049f9045a722732b414d496
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-09-05 23:44:41 +0530

    Asynchrony Await Example Updated …

--- 02.Week Asynchronous Javascript, Callbacks/Promise/02.Promises.js
+++ 02.Week Asynchronous Javascript, Callbacks/Promise/02.Promises.js
@@ -1,4 +1,21 @@
-// Write a function that takes an array of users 
-// as inputs and returns only the users who a
-// re more than 18 years old
-ee
+
+function random(){
+  console.log("Hello From random .. !!! ");
+  
+}
+let p = new Promise(random)
+console.log("Log before then",p);
+p.then((data)=>{
+    console.log(data);
+});
+
+// *******************
+
+
+aysnc function callMe(){ 
+    let data = await fetch("https://www.google.com")
+    return data
+}
+
+let callMeData = callMe()
+console.log( callMeData ) ;
