b3d7ca214ff1e4ec7a9f61e3492942d0586fd19d
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-09-07 22:12:49 +0530

    Promises Callback Callback Hell

--- 02.Week Asynchronous Javascript, Callbacks/03.asyncAwait.callbackHell/asyncAwait.callbackHell.js
+++ 02.Week Asynchronous Javascript, Callbacks/03.asyncAwait.callbackHell/asyncAwait.callbackHell.js
@@ -1,14 +1,79 @@
 
-console.log("hello");
 
-const fs = require("fs");
+// callback hell 
 
-let readFilefunction = new Promise((resolve,reject)=>{
-   resolve(contents = fs.readFileSync("a.txt", "utf-8"))
-})
+/* 
+Q: Write code that
+logs hi after 1 second
+logs hello 3 seconds after step 1
+logs hello there 5 seconds after step 2
+ 
+*/
 
-readFilefunction.then((data)=>{
-   console.log(data ,typeof(data))  // string
-   let trimData =  (data).trim()
-   console.log(trimData);
-})
+setTimeout(function () {
+   console.log("1 Second Passed");
+   setTimeout(function () {
+      console.log("2 Second Passed ");
+      setTimeout(function () {
+         console.log("3 Second Passed");
+      }, 5000);
+   }, 3000);
+}, 1000);
+
+// Alt solution (doesnt really have callback hell)
+
+function step3Done() {
+   console.log("hello there");
+ }
+ 
+ function step2Done() {
+   console.log("hello");
+   setTimeout(step3Done, 5000);
+ }
+ 
+ function step1Done() {
+   console.log("hi");
+   setTimeout(step2Done, 3000);
+ }
+ 
+ setTimeout(step1Done, 1000);
+ 
+//  Promisified version
+
+function setTimeoutPromisified(ms) {
+   return new Promise((resolve) => setTimeout(resolve, ms));
+ }
+ 
+ setTimeoutPromisified(1000).then(function () {
+   console.log("hi");
+   setTimeoutPromisified(3000).then(function () {
+     console.log("hello");
+     setTimeoutPromisified(5000).then(function () {
+       console.log("hello there");
+     });
+   });
+ });
+ 
+//  Async await syntax
+
+/* Write code that
+logs hi after 1 second
+logs hello 3 seconds after step 1
+logs hello there 5 seconds after step 2
+
+*/
+
+function setTimeoutPromisified(ms) {
+   return new Promise(resolve => setTimeout(resolve, ms));
+ }
+ 
+ async function solve() {
+    await setTimeoutPromisified(1000);
+    console.log("hi");
+    await setTimeoutPromisified(3000);
+    console.log("hello");
+    await setTimeoutPromisified(5000);
+    console.log("hi there");
+ }
+ 
+ solve();
