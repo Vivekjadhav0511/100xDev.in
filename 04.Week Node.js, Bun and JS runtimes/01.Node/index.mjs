9b69c64138f4a33eb39d9a8f534733cb72d728aa
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-10-10 16:33:06 +0530

    Node Chalk Library

--- 04.Week Node.js, Bun and JS runtimes/01.Node/index.mjs
+++ 04.Week Node.js, Bun and JS runtimes/01.Node/index.mjs
@@ -2,6 +2,7 @@
 // const chalk = require('chalk')
 
 import chalk from 'chalk'
+import fs from 'fs'
 
 console.log(chalk);
 
@@ -11,3 +12,8 @@ console.log(chalk.blue("Hello Javascript Node In Blue from Chalk"));
 console.log(chalk.blue('Hello, world!'));
 console.log(chalk.red.bold('This is an error message.'));
 console.log(chalk.green.underline('This is a success message.'));
+console.log(chalk.yellow("Hello Js"));
+
+fs.readFile("a.txt","utf-8",function(error,data){
+     console.log(data)
+})
