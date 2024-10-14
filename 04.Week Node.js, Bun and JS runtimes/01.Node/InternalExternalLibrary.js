e89f96f2a77472f740ea996b3cf922ad3711338c
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-10-14 22:59:13 +0530

    InternalExternal Library

--- 04.Week Node.js, Bun and JS runtimes/01.Node/InternalExternalLibrary.js
+++ 04.Week Node.js, Bun and JS runtimes/01.Node/InternalExternalLibrary.js
@@ -3,5 +3,28 @@
 const path = require("path") 
 console.log(__dirname); 
 
+/* 
+# npm
 
+The full form of **NPM** is **Node Package Manager**. 
+
+It is a package manager for JavaScript, primarily used for managing libraries and dependencies in Node.js projects. NPM allows developers to easily install, update, and manage packages of reusable code
+*/
+
+/* 
+# Internal Packages
+
+Node.js provides you some `packages` out of the box. Some common ones include
+
+1. fs - Filesystem
+2. path - Path related functions
+3. http - Create HTTP Servers (we’ll discuss this tomorrow)
+
+FS Package
+
+The fs (Filesystem) package is used to read, write, update contents on the filesystem.
+*/
+
+
+// 5.5.0 major minor patch version 
 
--- 04.Week Node.js, Bun and JS runtimes/01.Node/LeactureSlides.md
+++ 04.Week Node.js, Bun and JS runtimes/01.Node/LeactureSlides.md
@@ -3,3 +3,7 @@
 
  - [ Node.js, Bun and JS runtimes ...](https://petal-estimate-4e9.notion.site/Node-js-Bun-and-JS-runtimes-a09a41ccd61c4f498e55750c9a1c9b34)
 
+ ### You can read more about them on their npm page -
+
+ -[npm page](https://www.npmjs.com/package/chalk)
+
